b48aa2e5b2ad70f917fa0352fbeb5c7b9c03e4d7
Jessie Guzman <jessieguzman777@gmail.com>
2023-12-06 15:40:52 -0600

    commit tree e09e283e3bdbb8d995a2d947c4cdc5b059784ea3

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
-
-
+class Book
+  attr_accessor :title, :author, :page_count, :genre
+  attr_reader :turn_page
+  def initialize(title)
+    @title = title
+  end
+  def name(author)
+    @author = author
+  end
+  def number(page_count)
+    @page_count = page_count
+  end
+  def type(genre)
+    @genre = genre
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+class Shoe
+  attr_reader :brand, :cobble
+  attr_accessor :color, :size, :material, :condition
+  def initialize(brand)
+    @brand = brand
+  end
+  def shade(color)
+    @color = color
+  end
+  def dimension(size)
+    @size = size
+  end
+  def substance(material)
+    @material = material
+  end
+  def state(condition)
+    @condition = condition
+  end
+  def cobble
+    @condition = "new"
+   puts "Your shoe is as good as new!"
+  end
+end
